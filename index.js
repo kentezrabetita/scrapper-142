ca4e9dd94396148dec3b73b01874dbab066199ce
kentezrabetita <kentezrabetita@gmail.com>
2022-07-11 17:29:10 +0800

    setup puppeteer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const puppeteer = require('puppeteer');
 require('dotenv').config();
 
 const app = express();
@@ -13,4 +14,15 @@ app.use(express.json());
 
 app.listen(3000, (req, res) => {
   console.log('🚀 Server is runnning!');
+});
+
+app.get('/login', (req, res) => {
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    await page.screenshot({ path: 'example.png' });
+
+    await browser.close();
+  })();
 });
